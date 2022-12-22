12a4c0b124cb0a0ab4d1a73598bc7afbb7dd8abb
DiPolRiera <85955701+DiPolRiera@users.noreply.github.com>
2022-12-22 09:31:39 +0100

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -19,25 +19,25 @@ float Calculator::sum(float sum1, float sum2)
 
  float Calculator::sub(float sub1, float sub2)
 {
-  Rslt = sub1 + sub2;
+  Rslt = sub1 - sub2;
   return Rslt;
  }
 
  float Calculator::mult(float mult1, float mult2)
 {
-  Rslt = mult1 + mult2;
+  Rslt = mult1 * mult2;
   return Rslt;
  }
 
   float Calculator::divi(float divi1, float divi2)
 {
-  Rslt = divi1 + divi2;
+  Rslt = divi1 / divi2;
   return Rslt;
  }
 
  float Calculator::rest(float rest1, float rest2)
 {
-  Rslt = rest1 + rest2;
+  Rslt = rest1 % rest2;
   return Rslt;
  }
 
